d7218db88bb02c7c676da5be1080035dd2540b45
ayoubzoubirii <ayoub@zoubiri.com>
2023-03-16 11:27:25 +0100

    p2 v1

--- Band Name Generator/p1.py
+++ Band Name Generator/p1.py
@@ -3,4 +3,5 @@ def generator () :
     city = input("what's name of the city you grow up in ")
     pet = input("what't is pet's name ")
     print("Your band name could be "+city+" "+pet)
+    
 generator ()
