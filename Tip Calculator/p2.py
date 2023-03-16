f0944c8840fd86e19238d52a1307f1a29c9f0202
ayoubzoubirii <ayoub@zoubiri.com>
2023-03-16 21:10:43 +0100

    p2 v2

--- Tip Calculator/p2.py
+++ Tip Calculator/p2.py
@@ -2,14 +2,7 @@ def calc ():
     print("Welecome to the tip calculator")
     total_bill = float(input("What was the total bill ?"))
     num_people = int(input("How many people to split the bill ?"))
-    percentage = int(input("what percentage tip would like to give ? 10 , 12 or 15 ? "))
-    if percentage == 10 : 
-        pay = (percentage*(total_bill/100) + total_bill ) / num_people 
-        print("Each person should pay :" + str(round(float(pay),2)))
-    elif percentage == 12 : 
-        pay = (percentage*(total_bill/100) + total_bill ) / num_people 
-        print("Each person should pay :" + str(round(float(pay),2)))
-    elif percentage == 15 : 
-        pay = (percentage*(total_bill/100) + total_bill ) / num_people 
-        print("Each person should pay :" + str(round(float(pay),2)))    
+    percentage = int(input("what percentage tip would like to give ?  "))  
+    pay = (percentage*(total_bill/100) + total_bill ) / num_people 
+    print("Each person should pay :" + str(round(float(pay),2)))    
 calc()
